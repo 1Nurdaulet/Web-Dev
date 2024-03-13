f78f6281742880a93ea311ba99e7ef2f593667be
1Nurdaulet <n_kushzhanov@kbtu.kz>
2024-03-13 21:05:30 +0600

    LAB

--- LLLAB5/src/app/product-list/product-list.component.ts
+++ LLLAB5/src/app/product-list/product-list.component.ts
@@ -1,6 +1,6 @@
-import {Component, OnInit} from '@angular/core';
-import {ProductService} from "../services/product.service";
-import {ActivatedRoute, Router} from "@angular/router";
+import { Component, OnInit } from '@angular/core';
+import { ProductService } from "../services/product.service";
+import { ActivatedRoute, Router } from "@angular/router";
 
 @Component({
   selector: 'app-product-list',
@@ -8,33 +8,42 @@ import {ActivatedRoute, Router} from "@angular/router";
   styleUrls: ['./product-list.component.scss']
 })
 export class ProductListComponent implements OnInit {
-  products: any[] = []
-  category: string = ""
+  products: any[] = [];
+  category: string = "";
 
-  constructor(private productService: ProductService,
-              private activatedRoute: ActivatedRoute,
-              private router: Router) {
-  }
+  constructor(
+    private productService: ProductService,
+    private activatedRoute: ActivatedRoute,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
-    this.category = this.activatedRoute.snapshot.params["category"]
+    this.category = this.activatedRoute.snapshot.params["category"];
     const category = this.productService.categories.find(item => item.name === this.category);
-if (category) {
-    this.products = category.products;
-}
+    if (category) {
+      this.products = category.products;
+    } else {
+      console.error(`Category '${this.category}' not found.`);
+      // Handle the case where the category is not found
+      // For example, you could redirect to an error page or display a message to the user
+    }
   }
 
   goToRealPage(i: number) {
-    this.router.navigate([`products/${this.category}/${i}`])
+    this.router.navigate([`products/${this.category}/${i}`]);
   }
 
   removeProduct(i: number) {
-    this.products = this.products.filter(item => {
-      return item !== this.products[i]
-    });
-    const category = this.productService.categories.find(item => item.name === this.category);
-if (category) {
-    category.products = this.products;
-}
+    // Remove the product from the products array
+    this.products.splice(i, 1);
+    // Update the products array in the productService.categories
+    const categoryIndex = this.productService.categories.findIndex(item => item.name === this.category);
+    if (categoryIndex !== -1) {
+      this.productService.categories[categoryIndex].products = this.products;
+    } else {
+      console.error(`Category '${this.category}' not found.`);
+      // Handle the case where the category is not found in the productService.categories array
+    }
   }
 }
+
